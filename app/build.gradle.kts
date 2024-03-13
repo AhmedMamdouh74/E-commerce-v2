6583c038b48a20797b0d4d5cf0138bd2d0587234
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-03-13 15:42:39 +0200

    adding third party libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
+    // third party libraries
+    implementation("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
 }
