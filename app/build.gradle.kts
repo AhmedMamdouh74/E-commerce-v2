84d13f3fd03dc43e89237d6e234ece6a3b3c1bed
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-03-16 17:06:58 +0200

    adding navigation component dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,10 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics")
     // third party libraries
     implementation("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
+    // navigation component
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
 }
