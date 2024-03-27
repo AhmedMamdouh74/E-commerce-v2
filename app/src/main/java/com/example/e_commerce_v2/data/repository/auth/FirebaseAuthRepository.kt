1657d86e234c6ec8e70ecec6f6f2c4e09f323b22
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-03-27 17:19:26 +0200

    FirebaseAuthRepository and FirebaseAuthRepositoryImpl

---
+++ app/src/main/java/com/example/e_commerce_v2/data/repository/auth/FirebaseAuthRepository.kt
@@ -0,0 +1,13 @@
+package com.example.e_commerce_v2.data.repository.auth
+
+import com.example.e_commerce_v2.data.models.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface FirebaseAuthRepository {
+    suspend fun loginWithEmailAndPassword(
+        email: String, password: String
+    ): Flow<Resource<String>>
+
+    suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>>
+
+}
