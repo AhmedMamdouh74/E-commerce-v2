f76ddb4306bcdd2ead74d5b9921572b51cf1e144
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-03-27 17:17:44 +0200

    adding wrapper class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-crashlytics")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     // third party libraries
     implementation("com.github.pwittchen:reactivenetwork-rx2:3.0.8")
     // navigation component
