ac883c6b36e43ac3c875034e49b53c5a17f8d654
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-04-26 17:44:36 +0300

    add function login() to handle firebase login requests

--- app/src/main/java/com/example/e_commerce_v2/data/repository/auth/FirebaseAuthRepositoryImpl.kt
+++ app/src/main/java/com/example/e_commerce_v2/data/repository/auth/FirebaseAuthRepositoryImpl.kt
@@ -1,68 +1,98 @@
 package com.example.e_commerce_v2.data.repository.auth
 
 import com.example.e_commerce_v2.data.models.Resource
+import com.example.e_commerce_v2.data.models.user.AuthProvider
+import com.example.e_commerce_v2.data.models.user.UserDetailsModel
+import com.example.e_commerce_v2.utils.CrashlyticsUtils
+import com.example.e_commerce_v2.utils.LoginException
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 
-class FirebaseAuthRepositoryImpl(private val auth: FirebaseAuth = FirebaseAuth.getInstance()) :
+class FirebaseAuthRepositoryImpl(
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance(),
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+) :
     FirebaseAuthRepository {
     override suspend fun loginWithEmailAndPassword(
         email: String,
         password: String
-    ): Flow<Resource<String>> = flow {
+    ) = login(AuthProvider.EMAIL) { auth.signInWithEmailAndPassword(email, password).await() }
+
+    override suspend fun loginWithGoogle(idToken: String): Flow<Resource<UserDetailsModel>> =
+        login(AuthProvider.GOOGLE) {
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
+            auth.signInWithCredential(credential).await()
+        }
+
+    override suspend fun loginWithFacebook(token: String): Flow<Resource<UserDetailsModel>> =
+        login(AuthProvider.FACEBOOK) {
+            val credential = FacebookAuthProvider.getCredential(token)
+            auth.signInWithCredential(credential).await()
+        }
+
+    override fun logout() {
+        auth.signOut()
+    }
+
+    private suspend fun login(
+        provider: AuthProvider,
+        signInRequest: suspend () -> AuthResult,
+    ): Flow<Resource<UserDetailsModel>> = flow {
         try {
             emit(Resource.Loading())
-            // Got an ID token from Google. Use it to authenticate
-            // with Firebase.
-            val firebaseAuthResult = auth.signInWithEmailAndPassword(email, password).await()
-            firebaseAuthResult.user?.let { user ->
-                emit(Resource.Success(user.uid)) // emit the user ID
+            // perform firebase auth sign in request
+            val authResult = signInRequest()
+            val userId = authResult.user?.uid
+
+            if (userId == null) {
+                val msg = "Sign in UserID not found"
+                logAuthIssueToCrashlytics(msg, provider.name)
+                emit(Resource.Error(Exception(msg)))
+                return@flow
+            }
+
+            // get user details from firestore
+            val userDoc = firestore.collection("users").document(userId).get().await()
+            if (!userDoc.exists()) {
+                val msg = "Logged in user not found in firestore"
+                logAuthIssueToCrashlytics(msg, provider.name)
+                emit(Resource.Error(Exception(msg)))
+                return@flow
+            }
+
+            // map user details to UserDetailsModel
+            val userDetails = userDoc.toObject(UserDetailsModel::class.java)
+            userDetails?.let {
+                emit(Resource.Success(userDetails))
             } ?: run {
-                emit(Resource.Error(Exception("User not found")))
+                val msg = "Error mapping user details to UserDetailsModel, user id = $userId"
+                logAuthIssueToCrashlytics(msg, provider.name)
+                emit(Resource.Error(Exception(msg)))
             }
         } catch (e: Exception) {
-            emit(Resource.Error(e))
+            logAuthIssueToCrashlytics(
+                e.message ?: "Unknown error from exception = ${e::class.java}", provider.name
+            )
+            emit(Resource.Error(e)) // Emit error
         }
     }
 
-    override suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>> = flow {
-        try {
-            emit(Resource.Loading())
-            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-            val firebaseAuthResult = auth.signInWithCredential(firebaseCredential).await()
-            firebaseAuthResult.user?.let { user ->
-                emit(Resource.Success(user.uid)) // emit the user ID
-            } ?: run {
-                emit(Resource.Error(Exception("User not found")))
-            }
-
-
-        } catch (e: Exception) {
-            emit(Resource.Error(e))
-        }
+    private fun logAuthIssueToCrashlytics(msg: String, provider: String) {
+        CrashlyticsUtils.sendCustomLogToCrashlytics<LoginException>(
+            msg,
+            CrashlyticsUtils.LOGIN_KEY to msg,
+            CrashlyticsUtils.LOGIN_PROVIDER to provider,
+        )
     }
 
-    override suspend fun loginWithFacebook(token: String): Flow<Resource<String>> = flow {
-        try {
-            emit(Resource.Loading())
-            val firebaseCredential = FacebookAuthProvider.getCredential(token)
-            val firebaseAuthResult= auth.signInWithCredential(firebaseCredential).await()
-            firebaseAuthResult.user?.let { user ->
-                emit(Resource.Success(user.uid)) // emit the user ID
-            } ?: run {
-                emit(Resource.Error(Exception("User not found")))
-            }
-
-        }catch (e: Exception){
-            emit(Resource.Error(e))
-        }
-    }
 
     companion object {
-        private const val TAG = "FirebaseAuthRepositoryI"
+        private const val TAG = "FirebaseAuthRepository"
     }
 }
