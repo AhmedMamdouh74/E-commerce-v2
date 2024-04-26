dfc5cfa30de9f987b940ad0b5afd7aa6ddb9c011
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-04-26 17:51:30 +0300

    use UserDetailsModel

--- app/src/main/java/com/example/e_commerce_v2/data/repository/auth/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/e_commerce_v2/data/repository/auth/FirebaseAuthRepository.kt
@@ -1,14 +1,16 @@
 package com.example.e_commerce_v2.data.repository.auth
 
 import com.example.e_commerce_v2.data.models.Resource
+import com.example.e_commerce_v2.data.models.user.UserDetailsModel
 import kotlinx.coroutines.flow.Flow
 
 interface FirebaseAuthRepository {
     suspend fun loginWithEmailAndPassword(
         email: String, password: String
-    ): Flow<Resource<String>>
+    ): Flow<Resource<UserDetailsModel>>
 
-    suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>>
-    suspend fun loginWithFacebook(token: String): Flow<Resource<String>>
+    suspend fun loginWithGoogle(idToken: String): Flow<Resource<UserDetailsModel>>
+    suspend fun loginWithFacebook(token: String): Flow<Resource<UserDetailsModel>>
+    fun logout()
 
 }
